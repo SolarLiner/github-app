cdf3bf7c8428015372c8a3ed23638820e9154f3e
Nathan Graule <solarliner@gmail.com>
2018-04-12 19:30:43 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# github_sync
+
+Stay in touch with your favorite projects.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/login.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  final loginController = new TextEditingController();
+  final passwordController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Sync for GitHub"),
+      ),
+      body: new Center(
+        child: new Padding(
+          padding: new EdgeInsets.all(32.0),
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(
+                  "Loging in to your GitHub account allows the app to fetch" +
+                      " your favorite repositories and push notifications."),
+              new Padding(
+                padding: new EdgeInsets.only(top: 24.0),
+                child: new Column(
+                  children: <Widget>[
+                    new TextField(
+                      controller: loginController,
+                      decoration: new InputDecoration(hintText: "Login"),
+                    ),
+                    new TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: new InputDecoration(hintText: "Password"),
+                    ),
+                  ],
+                ),
+              ),
+              new Padding(
+                padding: new EdgeInsets.only(top: 24.0),
+                child: new RaisedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      child: new AlertDialog(
+                        title: new Text("What you entered"),
+                        content: new Text(loginController.text + "\n" + passwordController.text),
+                      ),
+                    );
+                  },
+                  child: new Text("Login"),
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
